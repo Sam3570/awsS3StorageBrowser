a6aad7fa73e998e64a8fb8d51ce996942698960f
Sam3570 <sameersayed3570@gmail.com>
2025-08-29 16:52:30 +0530

    test-18

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,18 +1,8 @@
-import { defineStorage } from '@aws-amplify/backend';
-
 export const storage = defineStorage({
   name: 'myS3Bucket',
   access: (allow) => ({
     'public/*': [
-      allow.group('Users').to(['read']),                  // Users → read
-      allow.group('Admins').to(['read', 'write', 'delete']) // Admins → full access
-    ],
-    'protected/{entity_id}/*': [
       allow.authenticated.to(['read']),
-      allow.entity('identity').to(['read', 'write', 'delete'])
-    ],
-    'private/{entity_id}/*': [
-      allow.entity('identity').to(['read', 'write', 'delete'])
     ]
-  }),
+  })
 });
