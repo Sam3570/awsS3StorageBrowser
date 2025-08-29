17786009ef3131235705e05e098fbddc6b139f6d
Sam3570 <sameersayed3570@gmail.com>
2025-08-29 14:43:54 +0530

    test-11

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -4,7 +4,7 @@ export const storage = defineStorage({
   name: 'myS3Bucket',
   access: (allow) => ({
     'public/*': [
-      allow.authenticated.to(['read',]),
+      allow.authenticated.to(['read']),
       allow.group('Admins').to(['read', 'write'])
   })
 });
