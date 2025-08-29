85e2d686de329ecf2f162f04358c0f5778f0885b
Sam3570 <sameersayed3570@gmail.com>
2025-08-29 13:43:51 +0530

    test-3

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -7,12 +7,5 @@ export const storage = defineStorage({
       allow.guest.to(['read']),
       allow.authenticated.to(['read', 'write', 'delete']),
     ],
-    'protected/{entity_id}/*': [
-      allow.authenticated.to(['read']),
-      allow.entity('identity').to(['read', 'write', 'delete'])
-    ],
-    'private/{entity_id}/*': [
-      allow.entity('identity').to(['read', 'write', 'delete'])
-    ]
   })
 });
