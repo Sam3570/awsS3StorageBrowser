e14b6a2e86beb063fc25ced84fcc98d5746f411b
Sam3570 <sameersayed3570@gmail.com>
2025-08-30 01:48:46 +0530

    test-26

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,31 +1,12 @@
-// import { defineBackend } from '@aws-amplify/backend';
-// import { auth } from './auth/resource.js';
-// import { data } from './data/resource.js';
-// import { storage } from './storage/resource';
+import { defineBackend } from '@aws-amplify/backend';
+import { auth } from './auth/resource.js';
+import { data } from './data/resource.js';
+import { storage } from './storage/resource';
 
-// defineBackend({
-//   auth,
-//   data,
-//   storage
-// });
-
-// const { cfnUserPool } = backend.auth.resources.cfnResources
-// // an empty array denotes "email" and "phone_number" cannot be used as a username
-// cfnUserPool.usernameAttributes = []
+defineBackend({
+  auth,
+  data,
+  storage
+});
 
 
-
-// import { defineBackend } from "@aws-amplify/backend"
-// import { auth } from "./auth/resource"
-// import { data } from "./data/resource"
-// import { storage } from './storage/resource';
-
-// const backend = defineBackend({
-//   auth,
-//   data,
-//   storage
-// })
-
-// const { cfnUserPool } = backend.auth.resources.cfnResources
-// // an empty array denotes "email" and "phone_number" cannot be used as a username
-// cfnUserPool.usernameAttributes = []
