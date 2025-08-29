f7d9d306b311c2ec761965fe5bce6b44aa601161
Sam3570 <sameersayed3570@gmail.com>
2025-08-29 14:37:04 +0530

    test-9

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,10 +1,27 @@
+// import { defineStorage } from '@aws-amplify/backend';
+
+// export const storage = defineStorage({
+//   name: 'myS3Bucket',
+//   access: (allow) => ({
+//     'public/*': [
+//       allow.guest.to(['read']),
+//       allow.authenticated.to(['read', 'write', 'delete']),
+//     ],
+//     'protected/{entity_id}/*': [
+//       allow.authenticated.to(['read']),
+//       allow.entity('identity').to(['read', 'write', 'delete'])
+//     ],
+//     'private/{entity_id}/*': [
+//       allow.entity('identity').to(['read', 'write', 'delete'])
+//     ]
+//   })
+// });
+
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
   name: 'myS3Bucket',
   access: (allow) => ({
-    '/*': [
-      allow.authenticated.to(['read','write']),
-    ],
-  })
+    '/*': allow.authenticated.to(['read', 'write']),  // no []
+  }),
 });
