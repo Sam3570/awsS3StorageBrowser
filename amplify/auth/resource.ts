1078a160b2cc954862c33cbeb8e6737d91e3136a
Sam3570 <sameersayed3570@gmail.com>
2025-08-31 02:32:22 +0530

    userAttributes-9rd try

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,20 +1,37 @@
-import { defineAuth } from '@aws-amplify/backend';
+// import { defineAuth } from '@aws-amplify/backend';
+
+// export const auth = defineAuth({
+//   loginWith: {
+//     email: true,
+    
+//   },
+    
+//  userAttributes: {
+//     displayName: {   // 👈 custom attribute (you can name it whatever you want)
+//       dataType: 'String',   // MUST be 'String' (capital S)
+//       mutable: true,
+//       required: false,
+//     },
+//   },
+//   groups: ['admin', 'auditor'],
+    
+// });
+
+
+import { defineAuth } from "@aws-amplify/backend";
 
 export const auth = defineAuth({
   loginWith: {
-    email: true,
-    
+    email: true, // login is still via email
   },
-    
   userAttributes: {
-    preferredUsername: {
-      dataType: 'string',  // ✅ required
+    "custom:display_name": {
+      dataType: "String",
       mutable: true,
       required: false,
+      maxLen: 50,
     },
   },
-  groups: ['admin', 'auditor'],
-    
+  groups: ["admin", "auditor"],
 });
-
 
