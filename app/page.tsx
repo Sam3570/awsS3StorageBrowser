972971c28a806f3585489acc78deedad84f69e39
Sam3570 <sameersayed3570@gmail.com>
2025-09-01 12:11:14 +0530

    check-4

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,59 @@
-
 "use client";
-import { Amplify, Auth } from "aws-amplify";
+
+import { useState, useEffect } from "react";
+import { generateClient } from "aws-amplify/data";
+import type { Schema } from "@/amplify/data/resource";
+import "./../app/app.css";
+import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
-import { Authenticator } from "@aws-amplify/ui-react";
-import { StorageBrowser } from "../components/StorageBrowser";
-import "./../app/app.css";
+import { Authenticator } from '@aws-amplify/ui-react';
+import { StorageBrowser } from '../components/StorageBrowser';
+import { fetchUserAttributes } from 'aws-amplify/auth';
 
 Amplify.configure(outputs);
 
+const client = generateClient<Schema>();
+
 export default function App() {
+  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+
+  function listTodos() {
+    client.models.Todo.observeQuery().subscribe({
+      next: (data) => setTodos([...data.items]),
+    });
+  }
+
+  useEffect(() => {
+    listTodos();
+  }, []);
+
+  function createTodo() {
+    client.models.Todo.create({
+      content: window.prompt("Todo content"),
+    });
+  }
   return (
     <Authenticator>
-      {({ signOut, user }) => {
-        let attributes: any = {};
-        console.log("User object:", user);
+  {({ signOut, user }) => {
+    let attributes: any = {};
+    console.log("User object:", user);  // 👈 Add here
+    
+    fetchUserAttributes().then(res => {
+      console.log("Fetched attributes:", res);
+      attributes = res;
+    });
 
-        fetchUserAttributes().then((res) => {
-          console.log("Fetched attributes:", res);
-          attributes = res;
-        });
-        
-        return (
-          <main>
-            <h1>Hello {attributes?.name}</h1>
-            <button onClick={signOut}>Sign out</button>
+    return (
+      <main>
+        <h1>Hello {user?.username}</h1>
+        <button onClick={signOut}>Sign out</button>
 
-            {/* StorageBrowser Component */}
-            <h2>Your Files</h2>
-            <StorageBrowser />
-          </main>
-        );
-      }}
-    </Authenticator>
+        <h2>Your Files</h2>
+        <StorageBrowser />
+      </main>
+    );
+  }}
+</Authenticator>
   );
 }
