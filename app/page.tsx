7ba029f8e0154a944237ba5d86388443d7efa236
Sam3570 <sameersayed3570@gmail.com>
2025-09-01 11:12:44 +0530

    userAttributes-ui-43th try

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,44 @@
+// "use client";
+// import { Amplify, Auth } from "aws-amplify";
+// import outputs from "@/amplify_outputs.json";
+// import "@aws-amplify/ui-react/styles.css";
+// import { Authenticator } from "@aws-amplify/ui-react";
+// import { StorageBrowser } from "../components/StorageBrowser";
+// import "./../app/app.css";
+
+// Amplify.configure(outputs);
+
+// export default function App() {
+//   return (
+//     <Authenticator>
+//       {({ signOut, user }) => {
+//         let attributes: any = {};
+//         console.log("User object:", user);
+
+//         fetchUserAttributes().then((res) => {
+//           console.log("Fetched attributes:", res);
+//           attributes = res;
+//         });
+
+//         return (
+//           <main>
+//             <h1>Hello {attributes?.username}</h1>
+//             <button onClick={signOut}>Sign out</button>
+
+//             {/* StorageBrowser Component */}
+//             <h2>Your Files</h2>
+//             <StorageBrowser />
+//           </main>
+//         );
+//       }}
+//     </Authenticator>
+//   );
+// }
+
+
 "use client";
-import { Amplify, Auth } from "aws-amplify";
+import { useEffect, useState } from "react";
+import { Amplify, fetchUserAttributes } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 import { Authenticator } from "@aws-amplify/ui-react";
@@ -9,13 +48,30 @@ import "./../app/app.css";
 Amplify.configure(outputs);
 
 export default function App() {
+  const [attributes, setAttributes] = useState<any>({});
+
+  useEffect(() => {
+    const getAttributes = async () => {
+      try {
+        const res = await fetchUserAttributes();
+        console.log("Fetched attributes:", res);
+        setAttributes(res);
+      } catch (error) {
+        console.error("Error fetching user attributes:", error);
+      }
+    };
+
+    getAttributes();
+  }, []);
+
   return (
     <Authenticator>
       {({ signOut, user }) => {
+        console.log("User object:", user);
 
         return (
           <main>
-            <h1>Hello {user?.signInDetails.loginId}</h1>
+            <h1>Hello {attributes?.name || user?.attributes?.name}</h1>
             <button onClick={signOut}>Sign out</button>
 
             {/* StorageBrowser Component */}
